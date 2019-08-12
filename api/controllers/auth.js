742f81943d939376f06d5ba44ef5affb2e57e0eb
nbuikema <46764298+nbuikema@users.noreply.github.com>
2019-08-12 13:54:21 -0500

    added user signup validation

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -17,3 +17,18 @@ exports.signup = (req, res) => {
         res.json({user: user});
     });
 };
+
+exports.signupValidator = (req, res, next) => {
+    req.check('first_name', 'First name is required.').notEmpty();
+    req.check('last_name', 'Last name is required.').notEmpty();
+    req.check('email', 'Email must contain @ symbol.').matches(/.+\@.+\..+/);
+    req.check('password', 'Password is required.').notEmpty();
+    req.check('password', 'Password must contain at least 6 characters.').isLength({min: 6});
+    req.check('password', 'Password must contain a number.').matches(/\d/);
+    const errors = req.validationErrors();
+    if(errors) {
+        const firstError = errors.map(error => error.msg)[0];
+        return res.status(400).json({error: firstError});
+    }
+    next();
+};
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const {helloWorld, signup} = require('../controllers/auth');
+const {helloWorld, signup, signupValidator} = require('../controllers/auth');
 
 router.get('/auth', helloWorld);
-router.post('/auth/signup', signup);
+router.post('/auth/signup', signupValidator, signup);
 
 module.exports = router;
