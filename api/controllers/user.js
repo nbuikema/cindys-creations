fd6520c23e58c0742465c516d1961f8ec6d91210
nbuikema <46764298+nbuikema@users.noreply.github.com>
2019-08-13 13:15:27 -0500

    removed useless user delete callback

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,13 +1,5 @@
 const User = require("../models/user");
 
-exports.helloWorldAuth = (req, res) => {
-    res.send('hello world user routes authorized user');
-};
-
-exports.helloWorldAdmin = (req, res) => {
-    res.send('hello world user routes admin');
-};
-
 exports.userById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
         if(err || !user) {
--- api/routes/user.js
+++ api/routes/user.js
@@ -4,8 +4,6 @@ const router = express.Router();
 const {helloWorldAuth, helloWorldAdmin, userById, readUser, updateUser, deleteUser} = require('../controllers/user');
 const {isSignedIn, isAuth, isAdmin} = require('../controllers/auth');
 
-router.get('/user/auth/:userId', isSignedIn, isAuth, helloWorldAuth);
-router.get('/user/admin/:userId', isSignedIn, isAuth, isAdmin, helloWorldAdmin);
 // createUser = auth-signup
 router.get('/user/:userId', isSignedIn, isAuth, readUser);
 router.put('/user/:userId', isSignedIn, isAuth, updateUser);
