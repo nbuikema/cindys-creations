b4ef354a15df2998534d7727c1cc1b66235f2bde
nbuikema <46764298+nbuikema@users.noreply.github.com>
2019-08-13 12:55:39 -0500

    added user read

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -19,3 +19,9 @@ exports.userById = (req, res, next, id) => {
         next();
     });
 };
+
+exports.readUser = (req, res) => {
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile);
+};
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {helloWorldAuth, helloWorldAdmin, userById} = require('../controllers/user');
+const {helloWorldAuth, helloWorldAdmin, userById, readUser, updateUser, deleteUser} = require('../controllers/user');
 const {isSignedIn, isAuth, isAdmin} = require('../controllers/auth');
 
 router.get('/user/auth/:userId', isSignedIn, isAuth, helloWorldAuth);
 router.get('/user/admin/:userId', isSignedIn, isAuth, isAdmin, helloWorldAdmin);
+// createUser = auth-signup
+router.get('/user/:userId', isSignedIn, isAuth, readUser);
 
 router.param('userId', userById);
 
