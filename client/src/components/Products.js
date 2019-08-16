0b8302b7e2631bb30b793af618935212786be711
Nick Buikema <nick.buikema@gmail.com>
2019-08-16 13:29:56 -0500

    test

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -26,11 +26,11 @@ const Products = () => {
         <div>
             <Navbar />
             <div className='container'>
-                <div className="col-8">
-                    <h2 className="mb-4">Products</h2>
-                    <div className="row">
+                <div className='col-8'>
+                    <h2 className='mb-4'>Products</h2>
+                    <div className='row'>
                         {products.map((product, i) => (
-                            <div key={i} className="col-4 mb-3">
+                            <div key={i} className='col-4 mb-3'>
                                 <ProductCard product={product} />
                             </div>
                         ))}
