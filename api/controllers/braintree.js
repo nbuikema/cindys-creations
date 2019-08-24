c8e74b71e7122b8c5908a49048e9c14c94d6aed3
nbuikema <46764298+nbuikema@users.noreply.github.com>
2019-08-24 17:54:15 -0500

    added braintree token route/controller

--- api/controllers/braintree.js
+++ api/controllers/braintree.js
@@ -1,7 +1,20 @@
-exports.helloWorldAuth = (req, res) => {
-    res.send('hello world braintree routes authorized user');
-};
+const User = require('../models/user');
+const braintree = require('braintree');
+require('dotenv').config();
 
-exports.helloWorldAdmin = (req, res) => {
-    res.send('hello world braintree routes admin');
+const gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    merchantId: process.env.BRAINTREE_MERCHANT_ID,
+    publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+    privateKey: process.env.BRAINTREE_PRIVATE_KEY
+});
+
+exports.generateToken = (req, res) => {
+    gateway.clientToken.generate({}, (err, response) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.send(response);
+        }
+    })
 };
--- api/routes/braintree.js
+++ api/routes/braintree.js
@@ -1,12 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const {helloWorldAuth, helloWorldAdmin} = require('../controllers/braintree');
+const {generateToken} = require('../controllers/braintree');
 const {isSignedIn, isAuth, isAdmin} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
 
-router.get('/braintree/auth/:userId', isSignedIn, isAuth, helloWorldAuth);
-router.get('/braintree/admin/:userId', isSignedIn, isAuth, isAdmin, helloWorldAdmin);
+router.get('/braintree/token/:userId', generateToken);
 
 router.param('userId', userById);
 
