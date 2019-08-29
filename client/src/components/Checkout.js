e771cb71d401df4cd32069d773a1c79fc3c0c0f6
nbuikema <46764298+nbuikema@users.noreply.github.com>
2019-08-29 12:09:23 -0500

    updated checkout page delivery information

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -9,14 +9,18 @@ const Checkout = () => {
     const [values, setValues] = useState({
         clientToken: null,
         instance: {},
+        email: '',
         address: '',
+        city: '',
+        state: '',
+        zip: '',
         error: '',
         success: false
     });
     const [cart, setCart] = useState([]);
     const [cartSize, setCartSize] = useState(0);
 
-    const {clientToken, address, error, success} = values;
+    const {clientToken, email, address, city, state, zip, error, success} = values;
 
     const changeCartSize = () => {
         setCartSize(readCart().length);
@@ -32,18 +36,22 @@ const Checkout = () => {
         });
     };
 
-    const initAddress = () => {
-        if(isAuthenticated() && isAuthenticated().user.address !== '') {
-            setValues({...values, address: isAuthenticated().user.address});
-        } else {
-            setValues({...values, address: 'a'});
+    const initUser = () => {
+        const {user} = isAuthenticated();
+        console.log(user)
+        if(isAuthenticated()) {
+            setValues({
+                ...values,
+                email: user.email,
+                address: user.address
+            });
         }
     };
 
     useEffect(() => {
         setCart(readCart());
         getToken();
-        initAddress();
+        initUser();
         changeCartSize();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [cartSize]);
@@ -102,27 +110,93 @@ const Checkout = () => {
         })
     };
 
-    const onChange = event => {
-        setValues({...values, address: event.target.value, error: ''});
+    const onChange = valueProp => event => {
+        setValues({...values, [valueProp]: event.target.value, error: ''});
     };
 
-    const showDeliveryAuth = () => address !== 'undefined' ? (
+    const showDelivery = () => (
         <div>
             <form>
                 <div className='form-group'>
-                    <label htmlFor='address'>Address</label>
-                    <input onChange={onChange} type='address' className='form-control' id='address' aria-describedby='address' value={address} />
+                    <label htmlFor='email'>Email</label>
+                    <input onChange={onChange('email')} value={email} type='email' className='form-control' id='email' aria-describedby='email' />
                 </div>
-            </form>
-        </div>
-    ) : '';
-
-    const showDeliveryNoAuth = () => (
-        <div>
-            <form>
                 <div className='form-group'>
                     <label htmlFor='address'>Address</label>
-                    <input onChange={onChange} type='address' className='form-control' id='address' aria-describedby='address' />
+                    <input onChange={onChange('address')} value={address} type='text' className='form-control' id='address' aria-describedby='address' />
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <div className='form-group'>
+                            <label htmlFor='city'>City</label>
+                            <input onChange={onChange('city')} value={city} type='text' className='form-control' id='city' aria-describedby='city' />
+                        </div>
+                    </div>
+                    <div className='col'>
+                        <div className='form-group'>
+                            <label htmlFor='state'>State</label>
+                            <select onChange={onChange('state')} value={state} className='form-control'>
+                                <option>Select One</option>
+                                <option value="AL">Alabama</option>
+                                <option value="AK">Alaska</option>
+                                <option value="AZ">Arizona</option>
+                                <option value="AR">Arkansas</option>
+                                <option value="CA">California</option>
+                                <option value="CO">Colorado</option>
+                                <option value="CT">Connecticut</option>
+                                <option value="DE">Delaware</option>
+                                <option value="DC">District Of Columbia</option>
+                                <option value="FL">Florida</option>
+                                <option value="GA">Georgia</option>
+                                <option value="HI">Hawaii</option>
+                                <option value="ID">Idaho</option>
+                                <option value="IL">Illinois</option>
+                                <option value="IN">Indiana</option>
+                                <option value="IA">Iowa</option>
+                                <option value="KS">Kansas</option>
+                                <option value="KY">Kentucky</option>
+                                <option value="LA">Louisiana</option>
+                                <option value="ME">Maine</option>
+                                <option value="MD">Maryland</option>
+                                <option value="MA">Massachusetts</option>
+                                <option value="MI">Michigan</option>
+                                <option value="MN">Minnesota</option>
+                                <option value="MS">Mississippi</option>
+                                <option value="MO">Missouri</option>
+                                <option value="MT">Montana</option>
+                                <option value="NE">Nebraska</option>
+                                <option value="NV">Nevada</option>
+                                <option value="NH">New Hampshire</option>
+                                <option value="NJ">New Jersey</option>
+                                <option value="NM">New Mexico</option>
+                                <option value="NY">New York</option>
+                                <option value="NC">North Carolina</option>
+                                <option value="ND">North Dakota</option>
+                                <option value="OH">Ohio</option>
+                                <option value="OK">Oklahoma</option>
+                                <option value="OR">Oregon</option>
+                                <option value="PA">Pennsylvania</option>
+                                <option value="RI">Rhode Island</option>
+                                <option value="SC">South Carolina</option>
+                                <option value="SD">South Dakota</option>
+                                <option value="TN">Tennessee</option>
+                                <option value="TX">Texas</option>
+                                <option value="UT">Utah</option>
+                                <option value="VT">Vermont</option>
+                                <option value="VA">Virginia</option>
+                                <option value="WA">Washington</option>
+                                <option value="WV">West Virginia</option>
+                                <option value="WI">Wisconsin</option>
+                                <option value="WY">Wyoming</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className='col'>
+                        <div className='form-group'>
+                            <label htmlFor='zip'>Zip Code</label>
+                            <input onChange={onChange('zip')} value={zip} type='text' className='form-control' id='zip' aria-describedby='zip' />
+                        </div>
+                    </div>
                 </div>
             </form>
         </div>
@@ -154,7 +228,7 @@ const Checkout = () => {
     return (
         <div className='container'>
             {isAuthenticated() ? (
-                (address && clientToken) || success ? (
+                clientToken && email || success ? (
                     <div>
                         <h2>Need to make changes? <Link to='/cart'>Go back to cart</Link></h2>
                         <div className='row'>
@@ -162,7 +236,7 @@ const Checkout = () => {
                                 <h2>Total To Be Charged: ${cartTotal()}</h2>
                                 {showError()}
                                 {showSuccess()}
-                                {showDeliveryAuth()}
+                                {showDelivery()}
                                 {showDropIn()}
                             </div>
                             <div className='col-3'>
@@ -171,27 +245,32 @@ const Checkout = () => {
                             </div>
                         </div>
                     </div>
+                    ) : (
+                        <Loader />
+                    )
                 ) : (
-                    <Loader />
-                )
-            ) : (
-                <div>
-                    <h2>Need to make changes? <Link to='/cart'>Go back to cart</Link></h2>
-                    <div className='row'>
-                        <div className='col-9'>
-                            <h2>Total To Be Charged: ${cartTotal()}</h2>
-                            {showError()}
-                            {showSuccess()}
-                            {showDeliveryNoAuth()}
-                            {showDropIn()}
-                        </div>
-                        <div className='col-3'>
-                            {showCart()}
-                            {emptyCart()}
+                clientToken || success ? (
+                    <div>
+                        <h2>Need to make changes? <Link to='/cart'>Go back to cart</Link></h2>
+                        <div className='row'>
+                            <div className='col-9'>
+                                <h2>Total To Be Charged: ${cartTotal()}</h2>
+                                {showError()}
+                                {showSuccess()}
+                                {showDelivery()}
+                                {showDropIn()}
+                            </div>
+                            <div className='col-3'>
+                                {showCart()}
+                                {emptyCart()}
+                            </div>
                         </div>
                     </div>
-                </div>
-            )}
+                    ) : (
+                        <Loader />
+                    )
+                )
+            }
         </div>
     );
 };
