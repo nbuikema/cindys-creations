2fd9345d5c069689e11ff18a8cdd6a33052f32c3
nbuikema <46764298+nbuikema@users.noreply.github.com>
2019-09-01 15:19:50 -0500

    added navbar collapse on click

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,20 +6,24 @@ const Navbar = ({history}) => (
     <div>
         <nav className='navbar navbar-expand-lg navbar-light bg-light'>
             <div className='container'>
-                <Link className='navbar-brand' to='/'>
-                    Cindy's Creations
-                </Link>
+                <ul className='navbar-nav mr-auto mt-lg-0'>
+                    <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
+                        <Link className='navbar-brand' to='/'>
+                            Cindy's Creations
+                        </Link>
+                    </li>
+                </ul>
                 <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarToggler' aria-controls='navbarToggler' aria-expanded='false' aria-label='Toggle Navigation'>
                     <span className='navbar-toggler-icon'></span>
                 </button>
                 <div className='collapse navbar-collapse' id='navbarToggler'>
                     <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
-                        <li className='nav-item'>
+                        <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
                             <Link className='nav-link' to='/products'>
                                 Products
                             </Link>
                         </li>
-                        <li className='nav-item'>
+                        <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
                             <Link className='nav-link' to='/contact'>
                                 Contact
                             </Link>
@@ -27,17 +31,17 @@ const Navbar = ({history}) => (
                     </ul>
                     {!isAuthenticated() && (
                         <ul className='navbar-nav ml-auto mt-2 mt-lg-0'>
-                            <li className='nav-item'>
+                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
                                 <Link className='nav-link' to='/cart'>
                                     My Cart <sup>{numItemsInCart()}</sup>
                                 </Link>
                             </li>
-                            <li className='nav-item'>
+                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
                                 <Link className='nav-link' to='/signup'>
                                     Sign Up
                                 </Link>
                             </li>
-                            <li className='nav-item'>
+                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
                                 <Link className='nav-link' to='/signin'>
                                     Sign In
                                 </Link>
@@ -45,12 +49,12 @@ const Navbar = ({history}) => (
                         </ul>
                     )} {isAuthenticated() && (
                         <ul className='navbar-nav ml-auto mt-2 mt-lg-0'>
-                            <li className='nav-item'>
+                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
                                 <Link className='nav-link' to='/cart'>
                                     My Cart <sup>{numItemsInCart()}</sup>
                                 </Link>
                             </li>
-                            <li className='nav-item dropdown'>
+                            <li className='nav-item dropdown' data-toggle='collapse' data-target='.navbar-collapse.show'>
                                 <Link className='nav-link dropdown-toggle' to='/' id='navbarDropdown' role='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
                                     {`${isAuthenticated().user.first_name} ${isAuthenticated().user.last_name}`}
                                 </Link>
