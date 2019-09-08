b1a33eab785491c122e79951c47b9261391ee1b5
Nick Buikema <nick.buikema@gmail.com>
2019-09-07 20:55:56 -0500

    added landing page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='container'>
-            Home Page
+        <div>
+            <div className='home'></div>
+            <h1 className='home-heading'>Cindy's Creations</h1>
         </div>
     );
 };
