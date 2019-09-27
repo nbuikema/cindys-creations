e316bf11738770ee950e78ce24d3c76895f734f5
nbuikema <46764298+nbuikema@users.noreply.github.com>
2019-09-27 11:12:57 -0500

    change product model timestamps to manual dates

--- api/models/product.js
+++ api/models/product.js
@@ -29,8 +29,16 @@ const productSchema = new mongoose.Schema(
         sold: {
             type: Number,
             default: 0
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        updatedAt: {
+            type: Date,
+            default: Date.now
         }
-    }, {timestamps: true}
+    }
 );
 
 module.exports = mongoose.model('Product', productSchema);
